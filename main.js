a173226186c8f6782f8c1b0973554ab8b93aaeed
Flavio Andrade <foa12381@gmail.com>
2023-02-11 12:43:00 -0300

    feat/validForm- cleanField

--- main.js
+++ main.js
@@ -7,22 +7,28 @@ form.addEventListener('submit', function (e) {
     for (const element of form){
       item[element.name] = element.value
     }
-
    create(item)
+   clenItem()
 })
 
+function clenItem(){
+ form[0].value = ""
+ form[1].value = ""
+ form[0].focus()
+}
+
 function create({nome, quantidade}) {
-  console.table(quantidade, nome)
+  if (!nome || !quantidade) return null
 
   const li = document.createElement('li')
   li.classList.add('item')
 
   const quant = document.createElement('strong')
   quant.innerHTML = quantidade
-
   li.appendChild(quant)
-  li.innerHTML += nome
 
+  li.innerHTML += nome
 
   ulList.appendChild(li)
 }
+
